cc04ca44be1dbbbb883d26e37d2244e796ea732d
K Narayan Reddy <narayanreddy355@gmail.com>
2026-01-10 22:28:09 +0530

    add two link

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,6 +3,8 @@ import jwt from "jsonwebtoken"
 
 async function authMiddleware(req, res, next) {
   const token = req.cookies.token;
+  console.log(token)
+
   if (!token) {
     return res.status(401).json({ message: "Token not found in cookies" });
   }
